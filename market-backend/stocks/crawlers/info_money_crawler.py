94f1bab4dbffa91bd2b47c15619d0cca431912a1
João Ricardo Simplício Soares <joaoricardosimplicio16@gmail.com>
2021-08-05 09:18:38 -0300

    Encapsulating page extraction logic obtained from infomoney in block try exception

--- market-backend/stocks/crawlers/info_money_crawler.py
+++ market-backend/stocks/crawlers/info_money_crawler.py
@@ -13,22 +13,25 @@ def _get_info_money_page_stocks(url : str = INFO_MONEY_BOVESPA_URL) -> str:
 
 
 def _extract_stocks_from_info_money_page() -> dict:
-    date_now = datetime.now()
-    content_page = _get_info_money_page_stocks().content
-    html_soup = BeautifulSoup(content_page, "html.parser")
-    stocks_table = html_soup.find('table', id='high').find_all("tr")
-    stocks_table += html_soup.find("table", id="low").find_all("tr")
-    stocks = [{"code": stock.find_all("td")[0].text, 
-              "price": stock.find_all("td")[1].text,
-              "day_variation": stock.find_all("td")[2].text,
-              "day": date_now.day,
-              "month": date_now.month,
-              "year": date_now.year,
-              "hour": date_now.hour,
-              "minute": date_now.minute,
-              "second": date_now.second}
-            for stock in stocks_table if stock.find_all("td")]
-    return stocks
+    try:
+        date_now = datetime.now()
+        content_page = _get_info_money_page_stocks().content
+        html_soup = BeautifulSoup(content_page, "html.parser")
+        stocks_table = html_soup.find('table', id='high').find_all("tr")
+        stocks_table += html_soup.find("table", id="low").find_all("tr")
+        stocks = [{"code": stock.find_all("td")[0].text, 
+                "price": stock.find_all("td")[1].text,
+                "day_variation": stock.find_all("td")[2].text,
+                "day": date_now.day,
+                "month": date_now.month,
+                "year": date_now.year,
+                "hour": date_now.hour,
+                "minute": date_now.minute,
+                "second": date_now.second}
+                for stock in stocks_table if stock.find_all("td")]
+        return stocks
+    except AttributeError:
+        raise("We had problems when try extract stocks information")
 
 
 def execute() -> list:
